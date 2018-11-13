0fc7c9d1dfc9536d843f7447a3373855c46fde72
Valentin Montagne <valentin.montagne@epitech.eu>
2018-11-13 17:36:56 +0900

    Fixing undefined title and fix get method

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ module.exports = Blobs
 
 Blobs.prototype.createWriteStream = function(options, cb) {
   var self = this
-  
+
   if (!options) options = {}
-  
-  var filename = options.filename
+
+  var filename = options.key
   var mimeType
   if (filename) {
     mimeType = mime.lookup(filename)
@@ -38,18 +38,18 @@ Blobs.prototype.createWriteStream = function(options, cb) {
     },
     contentType: 'application/json'
   }
-  
+
   if (parentID) metaOpts.json.parents = [{
     "kind": "drive#fileLink",
     "id": parentID
   }]
-  
+
   // always start by refreshing the token to make sure it's active
   self.refreshToken(function(err, resp, body) {
     if (err) {
       return cb(err)
     }
-    
+
     self.request(metaOpts, function(err, resp, body) {
       if (err || resp.statusCode > 299) return cb(err || resp.headers)
       var parsed = url.parse(resp.headers.location, true)
@@ -57,28 +57,28 @@ Blobs.prototype.createWriteStream = function(options, cb) {
       upload(session)
     })
   })
-  
+
   var proxy = through()
-  
+
   return proxy
-  
+
   function upload(session) {
     var opts = {
       query: '?uploadType=resumable&upload_id=' + session
     }
-    
+
     var put = self.request(opts)
-    
+
     put.on('response', function(resp) {
       resp.pipe(handleUploadResponse())
     })
-    
+
     put.on('error', function(err) {
       cb(err)
     })
-    
+
     proxy.pipe(put)
-    
+
     function handleUploadResponse() {
       return concat(function(body) {
         var response = JSON.parse(body)
@@ -101,13 +101,13 @@ Blobs.prototype.addProperty = function(id, key, val, cb) {
     "key": key,
     "value": val
   }
-  
+
   var reqOpts = {
     url: baseURL + 'files/' + id + '/properties',
     json: hashProp,
     contentType: 'application/json'
   }
-  
+
   return this.request(reqOpts, cb)
 }
 
@@ -124,7 +124,7 @@ Blobs.prototype.createReadStream = function(opts) {
     })
     duplex.setReadable(req)
   })
-  
+
   return duplex
 }
 
@@ -146,7 +146,7 @@ Blobs.prototype.mkdir = function(filename, opts, cb) {
     contentType: 'application/json',
     base: opts.baseURL
   }
-  
+
   var put = self.request(reqOpts, cb)
 }
 
@@ -155,17 +155,17 @@ Blobs.prototype.request = function(opts, cb) {
   if (!opts) opts = {}
   var token = self.options.access_token
   if (!token) return cb(new Error('you must specify google token'))
-  
+
   var reqOpts = {
     method: opts.method || 'POST',
     url: (opts.base || uploadBaseURL) + 'files' + (opts.query || ''),
     headers: {
-     'Content-Type': opts.contentType || 'text/plain',
-     'Authorization': "Bearer " + token
+      'Content-Type': opts.contentType || 'text/plain',
+      'Authorization': "Bearer " + token
     },
     json: opts.json
   }
-  
+
   if (opts.url) reqOpts.url = opts.url
   return request(reqOpts, function(err, resp, body) {
     if (process.env['DEBUG']) {
@@ -184,7 +184,7 @@ Blobs.prototype.request = function(opts, cb) {
 
 Blobs.prototype.get = function(hash, cb) {
   var self = this
-  var query = "properties has { key='key' and value='" + hash + "' and visibility='PRIVATE' } and trashed = false"
+  var query = "title = '" + hash + "'";
   var reqOpts = {
     contentType: 'application/json',
     query: '?' + formEncoder.encode({q: query}),
@@ -192,7 +192,7 @@ Blobs.prototype.get = function(hash, cb) {
     base: baseURL,
     json: true
   }
-  
+
   return self.request(reqOpts, function(err, resp, results) {
     if (err || resp.statusCode > 299) return cb(results)
     cb(null, results.items[0])
@@ -201,14 +201,14 @@ Blobs.prototype.get = function(hash, cb) {
 
 Blobs.prototype.remove = function(opts, cb) {
   var self = this
-  
+
   self.get(opts.key, function(err, file) {
     if (err) return cb(err)
     var reqOpts = {
       method: 'DELETE',
       url: baseURL + 'files/' + file.id
     }
-  
+
     self.request(reqOpts, function(err, resp, results) {
       if (err || resp.statusCode > 299) return cb(results || new Error('could not delete'))
       cb(null)
